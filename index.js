8bf970d9a5279c9abb7456209b757970bbbf3c57
nciri <nciri@MAC-L24-0069.local>
2019-10-23 11:42:47 +0200

    adding eventlistener on document. console logging KeyXXX is pressed whenever left, right, top, , down on keyboard is pressed

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const gameStartElement = document.getElementById('start-game')
 gameStartElement.addEventListener('click', (event) => {
     alert('Game is under construction')
 })
+
+document.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 40) {
+            console.log('KeyDown is pressed')
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 38) {
+            console.log('KeyUp is pressed')
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 37) {
+            console.log('KeyLeft is pressed')
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 39) {
+            console.log('KeyRight is pressed')
+    }
+})
