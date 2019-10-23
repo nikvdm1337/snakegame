2e8ea88ba0ba5c1d064857fd13301fa357e09b6f
nciri <nciri@MAC-L24-0069.local>
2019-10-23 12:40:18 +0200

    created 2d gameboard with canvas

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const gameStartElement = document.getElementById('start-game')
+const gameStartElement = document.getElementById("start-game");
 
-gameStartElement.addEventListener('click', (event) => {
-    alert('Game is under construction')
-})
+gameStartElement.addEventListener("click", event => {
+  alert("Game is under construction");
+});
+
+// Create gameboard
+
+const canvas = document.getElementById('gameboard');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'lightblue';
+ctx.fillRect(10, 10, 500, 500);
 
 document.addEventListener("keydown", event => {
-    if (event.isComposing || event.keyCode === 229) {
-        return;
-    }
-    else if (event.keyCode === 40) {
-        console.log('KeyDown is pressed')
-    } else if (event.keyCode === 38) {
-        console.log('KeyUp is pressed')
-    } else if (event.keyCode === 37) {
-        console.log('KeyLeft is pressed')
-    } else if (event.keyCode === 39) {
-        console.log('KeyRight is pressed')
-    }
-})
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  } else if (event.keyCode === 40) {
+    console.log("KeyDown is pressed");
+  } else if (event.keyCode === 38) {
+    console.log("KeyUp is pressed");
+  } else if (event.keyCode === 37) {
+    console.log("KeyLeft is pressed");
+  } else if (event.keyCode === 39) {
+    console.log("KeyRight is pressed");
+  }
+});
