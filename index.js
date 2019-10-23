b1f39b32107da819b27f44136f49a54b759b41f7
nciri <nciri@MAC-L24-0069.local>
2019-10-23 11:53:25 +0200

    formatting code

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ gameStartElement.addEventListener('click', (event) => {
 
 document.addEventListener("keydown", event => {
     if (event.isComposing || event.keyCode === 40) {
-            console.log('KeyDown is pressed')
+        console.log('KeyDown is pressed')
     }
 })
 
 document.addEventListener("keydown", event => {
     if (event.isComposing || event.keyCode === 38) {
-            console.log('KeyUp is pressed')
+        console.log('KeyUp is pressed')
     }
 })
 
 document.addEventListener("keydown", event => {
     if (event.isComposing || event.keyCode === 37) {
-            console.log('KeyLeft is pressed')
+        console.log('KeyLeft is pressed')
     }
 })
 
 document.addEventListener("keydown", event => {
     if (event.isComposing || event.keyCode === 39) {
-            console.log('KeyRight is pressed')
+        console.log('KeyRight is pressed')
     }
 })
