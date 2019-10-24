c22f7fb0a318cb5b448f21c77556f25932301262
nciri <nciri@MAC-L24-0069.local>
2019-10-24 10:14:32 +0200

    render with console log, state implementation

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import { initializeGame } from './gamelogic'
+import { paint } from "./render";
+
+const state = {
+  x: 250,
+  y: 250,
+  width: 10,
+  height: 10
+};
+
+function bindControls() {
+  document.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 229) {
+      return;
+    } else if (event.code === "ArrowDown") {
+      state.y++;
+    } else if (event.code === "ArrowUp") {
+      state.y--;
+    } else if (event.code === "ArrowLeft") {
+      state.x--;
+    } else if (event.code === "ArrowRight") {
+      state.x++;
+    } else {
+      return;
+    }
+    paint(state);
+  });
+}
+
+const initializeGame = function() {
+  bindControls();
+  paint(state);
+};
 
 initializeGame();
---
+++ render.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("gameboard");
+const ctx = canvas.getContext("2d");
+
+export function paint(state) {
+  console.log("state", state);
+}
